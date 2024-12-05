fd137f18d833b46e48c0d922fa335b0d5f7cb49e
auriacagigal <161497070+auriacagigal@users.noreply.github.com>
2024-12-05 14:19:24 +0000

    pontoEdiLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,12 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt">
+      <Header />
+      
     </html>
   );
 }
